7e3775c14c8d149006fa344eb8acd3387e381447
Jonhvmp <jonhpaz08@gmail.com>
2024-04-12 06:29:30 -0300

    create table

--- SQL/projeto_login.sql
+++ SQL/projeto_login.sql
@@ -1,4 +1,4 @@
-
+SET time_zone = "+00:00";
 
 
 CREATE TABLE usuarios (
@@ -8,9 +8,15 @@ CREATE TABLE usuarios (
   senha VARCHAR(255) NOT NULL,  /* senha: Senha do usuário (não pode ser nula). */
   data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,  /* data_criacao: Data e hora de criação do registro do usuário. */
   data_atualizacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE    CURRENT_TIMESTAMP   /* data_atualizacao: Data e hora da última atualização do registro do usuário. */
+); ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE tokens (
+  id_token INT PRIMARY KEY AUTO_INCREMENT, /* id_token: Identificador único para cada token.  */
+  id_usuario INT NOT NULL,  /* id_usuario: ID do usuário associado ao token.  */
+  token VARCHAR(255) NOT NULL UNIQUE,   /* token: Valor do token de autenticação (único).  */
+  data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,   /* data_criacao: Data e hora de criação do token.  */
+  data_expiracao DATETIME NOT NULL  /* data_expiracao: Data e hora em que o token expira e se torna inválido.  */
 );
 
-
-
-
-
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
